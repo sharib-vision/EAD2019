11878b45ee4e0c44d164aa35172785682bce8942
sharibox <38819666+sharibox@users.noreply.github.com>
2019-01-17 14:05:10 +0000

    Update run_evaluation_semantic_test.sh

--- scripts/run_evaluation_semantic_test.sh
+++ scripts/run_evaluation_semantic_test.sh
@@ -13,8 +13,10 @@ echo 'dataDir is' $DATA_DIR
 
 #-------
 BASE_FOLDER=$CURRENT_DIR/evaluation_semantic
+RESULT_FOLDER=$CURRENT_DIR/semantic_EAD2019_results
+mkdir -p $RESULT_FOLDER
 
-python $BASE_FOLDER/semanticEval_dice_Jaccard.py --GT_maskImage $DATA_DIR/0000600_mask.tif --Eval_maskImage $DATA_DIR/0000600_mask.tif
+python $BASE_FOLDER/semanticEval_dice_Jaccard.py --GT_maskImage $DATA_DIR/0000600_mask.tif --Eval_maskImage $DATA_DIR/0000600_mask.tif --Result_dir $RESULT_FOLDER 
 
 # TODO: grep values from txt file and put it in json file
 echo " Semantic Evaluation complete !!!"
