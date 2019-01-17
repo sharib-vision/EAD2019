0996b9d5ad0ee223c181a01594cf09b275007e3a
sharib ali <sharib.ali@eng.ox.ac.uk>
2019-01-17 09:25:02 +0000

    setup file

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 17 09:19:34 2019
+
+@author: shariba
+"""
+
+from os import path
+from setuptools import setup, find_packages
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as fp:
+    requirements = [r.rstrip() for r in fp.readlines() if not r.startswith('#')]
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as fp:
+    long_description = fp.read()
+
+setup(
+    name='ead2019',
+    version='1.2',
+    description='Endoscopic artefact detection challenge (ISBI2019)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/sharibox/EAD2019',
+    author='EAD2019',
+    author_email='sharib.ali@eng.ox.ac.uk',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha, 4 - Beta, 5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='endoscopy artefact detection segmentation',
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=requirements,
+)
