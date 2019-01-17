8b2bc6cb3596954874f1e72c7d50f3e302364f19
sharib ali <sharib.ali@eng.ox.ac.uk>
2019-01-17 12:41:24 +0000

    small changes

--- scripts/run_evaluation_test.sh
+++ scripts/run_evaluation_test.sh
@@ -4,18 +4,28 @@
 # contact: sharib.ali@eng.ox.ac.uk
 
 # change me
-DATA_DIR=../mAP-IoU_testdata
 
+CURRENT_DIR=`pwd`
+cd $CURRENT_DIR
+
+#cd "$(CURRENT_DIR "$0")"
+
+DATA_DIR=$CURRENT_DIR/mAP-IoU_testdata
+
+echo 'dataDir is' $DATA_DIR
 #-------
-BASE_FOLDER=../evaluation_mAP-IoU
-CLASS_NAMES=../endo.names
-
-if [ -d "$RESULT_FOLDER" ]; then rm -Rf $RESULT_FOLDER; fi
+BASE_FOLDER=$CURRENT_DIR/evaluation_mAP-IoU
+CLASS_NAMES=$CURRENT_DIR/endo.names
 
 # change me (if you want to rename the folder to something else)
-RESULT_FOLDER=../mAP-IOU_EAD2019_results/
+RESULT_FOLDER=$CURRENT_DIR/mAP-IOU_EAD2019_results
+
+mkdir -p $RESULT_FOLDER
 
 # compute the mAP and IoU (Please note that weighted value will be scored, see challenge website for details, https://ead2019.grand-challenge.org/Evaluation/)
+
+#PARAMETERS=$BASE_FOLDER/compute_mAP_IoU.py \#$DATA_DIR/predicted\#$DATA_DIR/ground-truth\#$RESULT_FOLDER
+#echo $PARAMETERS
 
 python $BASE_FOLDER/compute_mAP_IoU.py $DATA_DIR/predicted $DATA_DIR/ground-truth $RESULT_FOLDER
 
