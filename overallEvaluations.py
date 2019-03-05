f04f5555991ec879b06af9f48bb3b8a40a046efb
sharib ali <sharib.ali@eng.ox.ac.uk>
2019-03-05 01:12:04 +0000

    small

--- overallEvaluations.py
+++ overallEvaluations.py
@@ -14,7 +14,7 @@ def get_args():
     parser = argparse.ArgumentParser(description="For EAD2019 challenge: semantic segmentation", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--detectionMetric", type=str, default="mAP-IOU_EAD2019_results/metrics_detection.json", help="json file for detection")
     parser.add_argument("--generalizationMetric", type=str, default="mAP-IOU_EAD2019_results/metrics_detection.json", help="json file for generalization")
-    parser.add_argument("--semantic_detection", type=str, default="mAP-IOU_EAD2019_results/metrics_detection.json", help="son file for segmentation")
+#    parser.add_argument("--semantic_detection", type=str, default="mAP-IOU_EAD2019_results/metrics_detection.json", help="son file for segmentation")
     parser.add_argument("--semanticMetric", type=str, default="evaluation_semantic/results/metrics_semantic.json", help="son file for segmentation")
     parser.add_argument("--caseType", type=int, default=3, help="please set 0: only for dection both balanced, 1: only for instance segmentation only, 2: for generalization, 3: for all tasks")
     parser.add_argument("--Result_dir", type=str, default="finalEvaluationScores", help="all evaluation scores used for grading")
@@ -34,7 +34,6 @@ if __name__ == '__main__':
     import os
     
     valArgs = get_args()
-    exists = os.path.isfile(valArgs.detectionMetric)
     mAP_d = 0
     mAP_s = 0
     IOU_d = 0
@@ -42,15 +41,18 @@ if __name__ == '__main__':
     semScore = 0
     mAP_g = 0
     score_d = 0
+    F2_score=0
+    score_g = 0
     debug = 0
-    
-    if exists:
-        data = read_json(valArgs.detectionMetric)
-        valAppend = []
-        for p in data["EADChallenge2019"].values():
-            valAppend.append(p)
-        scoreDetection = valAppend[2]['value']*0.01
-        if valArgs.caseType == 0 or valArgs.caseType == 3 or valArgs.caseType == 2:
+    """ case: Detection """
+    if valArgs.caseType == 0 or valArgs.caseType == 3 or valArgs.caseType == 2 or valArgs.caseType == 4 or valArgs.caseType == 5:
+        exists = os.path.isfile(valArgs.detectionMetric)
+        if exists:
+            data = read_json(valArgs.detectionMetric)
+            valAppend = []
+            for p in data["EADChallenge2019"].values():
+                valAppend.append(p)
+            scoreDetection = valAppend[2]['value']*0.01
             if debug:
                 print('final score is computed for detection, (task-I, EAD2019 challenge)')
                 print('your score is:', valAppend[2]['value']*0.01)
@@ -60,26 +62,20 @@ if __name__ == '__main__':
                 print('mean IOU:', valAppend[1]['value']*0.01)
                 print('~~~~~~~~~~~~~~~~~~~~~~~~E.O.F~~~~~~~~~~~~~~~~~~~~~~~~~~')
                 print('All scores are saved in json files, see dir:', valArgs.Result_dir)
-            
+        
             mAP_d = valAppend[0]['value']*0.01
             IOU_d = valAppend[1]['value']*0.01
             score_d = valAppend[2]['value']*0.01
-                
-        if valArgs.caseType == 1 or valArgs.caseType == 3:
-#            Detection has been removed from final submission for semantic
-            # first read the json file for detection
-#            data = read_json(valArgs.semantic_detection)
-#            valAppend = []
-#            for p in data["EADChallenge2019"].values():
-#                valAppend.append(p)
-#            if debug:
-#                print('final score computed for instance segmentation (task-II, EAD2019 challenge), please make sure that you have computed mAP before hand on the same data...')
-            
+        
+    """ case: Semantic """
+    if valArgs.caseType == 1 or valArgs.caseType == 3 or valArgs.caseType == 5:
+        exists = os.path.isfile(valArgs.semanticMetric)
+        if exists:
             data = read_json(valArgs.semanticMetric)
             valAppend_Semantic=[]
             for p in data["EADChallenge2019"].values():
                 valAppend_Semantic.append(p)
-                
+                    
             # compute scores
             finalScore = 0.75*valAppend_Semantic[3]['value'] + 0.25*valAppend_Semantic[2]['value']
             scoreSemantic = valAppend_Semantic[3]['value']
@@ -90,34 +86,32 @@ if __name__ == '__main__':
                 print('mean mAP alone:', valAppend[0]['value']*0.01)
                 print('mean semantic score alone:', scoreSemantic)
                 print('~~~~~~~~~~~~~~~~~~~~~~E.O.F~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-            
+        
             F2_score = valAppend_Semantic[2]['value']
             overlap = scoreSemantic
             semScore = finalScore
-            
-        if valArgs.caseType == 2 or valArgs.caseType == 3 or valArgs.caseType == 4:
+                
+            """ case: Generalization """
+    if  valArgs.caseType == 3 or valArgs.caseType == 4:
+        exists = os.path.isfile(valArgs.generalizationMetric)
+        if exists:
             data = read_json(valArgs.generalizationMetric)
-            valAppendGeneral = []
+            valGen = []
             for p in data["EADChallenge2019"].values():
-                valAppendGeneral.append(p)
-            
+                valGen.append(p)
+                
             if debug:
-                print('final score is computed only for generalization, (task-III, EAD2019 challenge)')
-                print('your score is:', valAppendGeneral[2]['value']*0.01)
                 print('~~~~~~~~~~~~~~~Complimentary informations~~~~~~~~~~~~~~~')
-                print('mean mAP:', valAppendGeneral[0]['value']*0.01)
-                print('mean IOU:', valAppendGeneral[1]['value']*0.01)
+                print('mean mAP:', valGen[0]['value']*0.01)
+                print('mean score_g:', valGen[1]['value']*0.01)
                 print('~~~~~~~~~~~~~~~E.O.F~~~~~~~~~~~~~~~')
-            
-#            print('computing generalization gap...')
-#            print('semantic gap:', (1- (valAppendGeneral[0]['value']*0.01-valAppend[0]['value']*0.01)))
-            # read json file from your mAP score from task I - your current mAP score on this data 
-    
-            mAP_g = valAppendGeneral[0]['value']*0.01
-    
-    else:
-        print('no multi-class artefact detection found, mAPs are required for scoring both segmentation and generalization tasks')
-        
+  
+            mAP_g = valGen[0]['value']
+            score_g = valGen[1]['value']
+#    
+#    else:
+#        print('no multi-class artefact detection found, mAPs are required for scoring both segmentation and generalization tasks')
+#        
     '''
     creating json file
     '''
@@ -143,7 +137,10 @@ if __name__ == '__main__':
                   "value": (semScore)
                 },
                 "mAP_g":{
-                  "value": (mAP_g),  
+                  "value": (mAP_g)
+                },
+                "dev_g":{
+                  "value": (score_g),  
                 }
                 
             }
